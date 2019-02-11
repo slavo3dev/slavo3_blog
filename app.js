f2f70b2ae142c618575ceb96720b897842eea93e
slavo@mimicom24.com <slavo@mimicom24.com>
2019-02-11 13:30:34 -0500

    adding

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const p = require("./passwords")
 
-console.log(p.PASSWORD)
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,6 +15,9 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+// Heroku env variables
+let password = p.PASSWORD || process.env.PASSWORD;
+let port = process.env.PORT || 3000;
 mongoose.connect(`mongodb://slavo3dev:${p.PASSWORD}@mimicom24-shard-00-00-wbdvr.mongodb.net:27017,mimicom24-shard-00-01-wbdvr.mongodb.net:27017,mimicom24-shard-00-02-wbdvr.mongodb.net:27017/slavoblog?ssl=true&replicaSet=MimiCom24-shard-0&authSource=admin&retryWrites=true`, {useNewUrlParser: true});
 
 const blogSchema =  { 
